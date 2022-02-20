9822b6e41fc6e50bb9b1f04913209c138bae4317
Rai <robertblake.hardy@outlook.com>
2022-02-20 01:17:19 -0100

    feat: add animation, update js script

--- app/app.js
+++ app/app.js
@@ -1,7 +1,28 @@
-const accordion = document.getElementsByClassName('card-accordion-section');
+// Select all accordion items
+const acc = document.querySelectorAll('.card-accordion-section');
 
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function() {
-        this.classList.toggle('active')
-    })
+// Iterate to add event listeners
+acc.forEach(item => {
+    item.addEventListener('click', function() {
+        // When it's clicked, loop through all the items
+        acc.forEach(el => {
+            // Close any open items
+            if (el.classList.contains('active')) {
+                closeAcc(el);
+                // If it's the one that was clicked and it's closed, open it
+            } else if (el === item) {
+                openAcc(el);
+            }
+        });
+    });
+});
+
+function closeAcc(el) {
+    el.classList.remove('active');
+    el.nextElementSibling.style.maxHeight = null;
+};
+
+function openAcc(el) {
+    el.classList.add('active');
+    el.nextElementSibling.style.maxHeight = el.nextElementSibling.scrollHeight + 'px';
 }
